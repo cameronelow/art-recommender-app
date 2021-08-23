fd27da2a14f6038a6fe1b03097db5e6536440303
Cameron Lowry <38968949+cameronelow@users.noreply.github.com>
2021-08-23 00:18:38 -0400

    added ionic

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {IonContent, IonToolbar, IonHeader, IonTitle, IonCard, IonCardTitle, IonCardContent} from "@ionic/react";
+
+export default () => (
+
+<IonContent>
+    <p>welcome to the art recommender</p>
+</IonContent>
+
+);
---
+++ app/javascript/packs/Index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import App from "../components/App";
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
